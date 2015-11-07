3957858f520dcc75b2ba0077aa889bdbb7cd8d97
J. Arturo Covarrubias <a.covar@gmail.com>
2015-11-06 22:30:23 -0600

    Fix issues

--- Capitulo1/codigo/ex5-server.js
+++ Capitulo1/codigo/ex5-server.js
@@ -1,12 +1,10 @@
 function manejarHttp(req,res){
-
-  res.writeHead(200,{"Content-type":"plain/text"});
-  res.end("BOOM!!!! SERVIDOR WEB ARRIBA ");
-
+  res.writeHead(200);
+  res.end("Hola Mundo");
 }
 
 var host = "localhost";
-var port = 8081;
+var port = 8006;
 
 var http = require("http");
-var servidor = http.createServer(menejoHttp).listen(port,host);
+var servidor = http.createServer(manejarHttp).listen(port,host);
